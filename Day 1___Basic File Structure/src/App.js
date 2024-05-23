675b44542c14890a52bea3d3067371d491f17c62
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2024-05-23 20:51:45 +0500

    Day 1

---
+++ Day 1___Basic File Structure/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+import GoalList from "./components/Goal_List/Goal_List";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">Course Goals</h1>
+      <GoalList/>
+    </div>
+  );
+}
+
+export default App;
