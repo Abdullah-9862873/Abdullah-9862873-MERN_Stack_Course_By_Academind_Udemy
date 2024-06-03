5b5782d79fc32b20370d599537983954e088d252
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2024-06-04 02:48:32 +0500

    Day 1

---
+++ Day 01___Basic File Structure/src/components/Goal_List/Goal_List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Goal_List.css";
+
+const Goal_List = () => {
+  return (
+    <ul className="list">
+      <li className="list1">Finish the Course</li>
+      <li className="list2">Learn All about the course main topics</li>
+      <li className="list3">Help other students in course Q and A</li>
+    </ul>
+  );
+};
+
+export default Goal_List;
