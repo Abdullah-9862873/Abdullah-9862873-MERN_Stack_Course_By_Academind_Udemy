cefbaa2ddbfd23c8c0b985991c41a4a97a74f329
Abdullah Sultan <82256525+Abdullah-9862873@users.noreply.github.com>
2024-06-04 02:54:30 +0500

    Day 10

---
+++ Day 10___ Replacing traditional MongoDB setup with Mongoose Library___Database connection concepts class 2/mongoose.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Product = require("./models/products");
+
+// Connection
+mongoose.connect(
+  "mongodb+srv://Abdullah:Sapphire%401@cluster0.bhhxoyj.mongodb.net/products"
+).then(() => {
+    console.log("Connected to database!");
+}).catch(() => {
+    console.log("Connection to database failed!");
+});
+
+const createProduct = async (req, res, next) => {
+  const createdProduct = new Product({
+    name: req.body.name,
+    price: req.body.price,
+  });
+  const result = await createdProduct.save();
+  res.json(result);
+};
+
+const getProduct = async (req,res,next) => {
+    const result = await Product.find().exec();
+    res.json(result);
+}
+
+module.exports.createProduct = createProduct;
+module.exports.getProduct = getProduct;
